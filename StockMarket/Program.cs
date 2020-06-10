bb0736e3f6761a9cd9c1e2e6467c1a3546c4a61e
Tran Quoc Trung <tranquoctrung.1610@gmail.com>
2020-06-10 16:32:23 +0700

    init commit

---
+++ StockMarket/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Web.Helpers;
+using WebSocketSharp;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ws = new WebSocket("ws://socket.vcsc.com.vn:7000/websocket"))
+            {
+                ws.OnMessage += (sender, e) =>
+                {
+                    Console.WriteLine(e.Data);
+                    if (!string.IsNullOrEmpty(e.Data))
+                    {
+                        dynamic data = JObject.Parse(e.Data);//Json.Decode(e.Data);
+                        string service = data.service;
+                        if (!string.IsNullOrEmpty(service) && service =="stockinfo")
+                        {
+                            var content = data.content;
+                            Console.WriteLine(service);
+                        }
+                    }
+                };
+
+                ws.OnError += (sender, e) =>
+                    Console.WriteLine("Error: " + e.Message);
+
+                ws.Connect();
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
